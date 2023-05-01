3c766517fe37b13d1d41f7c936eb5adad4007be9
Rey Garcia <jreygarcia24@gmail.com>
2023-05-01 09:26:41 +0800

    Boilerplate for Frontend Mentor Challenges.

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+useHead({
+  title: "Frontend Mentor | [Challenge Title] | Rey Garcia",
+  meta: [
+    {
+      name: "description",
+      content: "Challenge by Frontend Mentor. Coded by Rey Garcia.",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1.0",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="Main">
+    <!-- Add component here -->
+  </div>
+  <footer>
+    <div class="attribution">
+      <div>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+          >Frontend Mentor</a
+        >
+      </div>
+      <div>
+        Coded by
+        <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a>
+      </div>
+    </div>
+  </footer>
+</template>
